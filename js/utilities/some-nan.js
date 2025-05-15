ebdfbd755d61b560608182bd8c9d02f64b83dfd1
Raúl Valverde <raul@fullcircle.es>
2025-05-15 11:05:21 +0200

    some NaN

--- js/utilities/some-nan.js
+++ js/utilities/some-nan.js
@@ -1,3 +1,9 @@
+/**
+ * @param  {...number | number[]} values 
+ * @returns {boolean} true if some value is NaN
+ * @example
+ * if (someNaN(id, date.getTime(), eventId)) throw new Error('Invalid parameters')
+ */
 function someNaN(...values) {
-    return values.some(value => isNaN(value))
+    return values.flat().some(value => isNaN(value))
 }
